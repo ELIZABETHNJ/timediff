de3c6b0a2226b3063c861a2053b76d6bcb8c8a36
ELIZABETHNJ <elizabethnj1996@gmail.com>
2017-07-10 17:51:13 +0530

    Create Timediff.java

---
+++ Timediff.java
@@ -0,0 +1,21 @@
+# timediff
+import java.io.*;
+import java.util.*;
+public class Timediff
+{
+  public static void main(String args[])
+  {
+    Scanner sc=new Scanner(System.in);
+    int h1,m1,h2,m2,tmin1,tmin2=0,min;
+    System.out.println("Enter the first time hours and seconds");
+    h1=sc.nextInt();
+    m1=sc.nextInt();
+    tmin1=h1*60;
+    System.out.println("Enter the second time hours and seconds");
+    h2=sc.nextInt();
+    m2=sc.nextInt();
+    tmin2=h2*60;
+    min=tmin2-tmin1;
+    System.out.println("The minute difference between 2 time is : "+Math.abs(min));
+  }
+}
